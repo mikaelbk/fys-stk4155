cf348c4026ab2a0305e9270ee1a125dbfa39c882
Mikael B. Kiste <mikael.kiste@gmail.com>
2018-08-29 11:31:16 +0200

    doing homework1

--- homework1/homework1.py
+++ homework1/homework1.py
@@ -1,1 +1,14 @@
 from numpy import *
+from matplotlib.pyplot import *
+
+x = np.random.rand(100,1)
+y = 5*x*x+0.1*np.random.randn(100,1)
+def funcY(x):
+	return 5*x*x+0.1
+
+
+scatter(x,y)
+
+show()
+
+print(y)
