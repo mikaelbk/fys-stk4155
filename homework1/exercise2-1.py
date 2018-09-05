ecddcadfbdfd10c730a4aeeddd45dae4e669a7a3
Mikael B. Kiste <mikael.kiste@gmail.com>
2018-09-05 14:43:17 +0200

    minor changes in 2.1: reduced lines on making the X matrix. added comments

--- homework1/exercise2-1.py
+++ homework1/exercise2-1.py
@@ -1,27 +1,33 @@
 from numpy import *
 from matplotlib.pyplot import *
 
+# a, b and c are the polynomial coefficients. n is number of datapoints
 a = 1.5
 b = -7
 c = 12
 n = 100
 
+# function definition of second order polynomial
 def pol2(x):
 	return a*x**2 + b*x + c
 
+# x is independent variable while y is response variable (with noise)
 x = sort(20*(random.rand(n)-0.5))
 y = pol2(x) + (10*max(x)*(random.rand(n)-0.5))
 
-xb = zeros([3,n])
-xb[0] = x**2
-xb[1] = x
-xb[2] = ones(n)
-xb = xb.T
+# defining the X matrix as [[x^2],[x],[1]]
+# where each of the three elements are column vectors
+xb = array([x**2,x,ones(n)]).T
 
+# here's where the magic happens
+# through linear algebra this should give coefficients of the polynomial
+# based on minimizing the sum of the square of residuals
 beta = linalg.inv(xb.T.dot(xb)).dot(xb.T).dot(y)
 
+# predicted y values based on the coefficients found (beta)
 yPredict = xb.dot(beta)
 
+#plotting
 plot(x,pol2(x),'b',label = "measurements w/o noise")
 plot(x,y,'bo',label = "measurements")
 plot(x,yPredict,'r', label = "regression")
